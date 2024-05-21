9476a5bbb053b84d00516c4fee5a50c8d1089477
André Pereira <andreporteira@gmail.com>
2024-05-21 14:16:33 -0300

    Css

--- src/fetchPokemon.jsx
+++ src/fetchPokemon.jsx
@@ -1,6 +1,7 @@
 export default async function fetchPokemon(id) {
   try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    console.log(`https://pokeapi.co/api/v2/pokemon/${id}`)
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -22,18 +22,23 @@ function Pokemon() {
   if (!pokemonData) {
     return null; // or any fallback UI
   }
+  console.log(pokemonData.id)
   const capitalizedPokemonName = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)
   return (
-    <div>
-      <h1>{capitalizedPokemonName}</h1>
-      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.order}.png`} alt='pokemon'></img>
-      <ul>
-        <li><strong>Pokedex #:</strong> {pokemonData.order}</li>
-        <li><strong>Height:</strong> {pokemonData.height * 10} cm</li>
-        <li><strong>Weight:</strong> {pokemonData.weight * 0.1} kg</li>
-      </ul>
+    <div className="flex">
+       <div className="cartao-pokemon inside">
+        <h1>{capitalizedPokemonName}</h1>
+        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`} alt='pokemon'></img>
+        <ul>
+          <li><strong>Pokedex #:</strong> {pokemonData.id}</li>
+          <li><strong>Height:</strong> {pokemonData.height * 10} cm</li>
+          <li><strong>Weight:</strong> {pokemonData.weight * 0.1} kg</li>
+        </ul>
+      </div>
     </div>
   );
 }
+
+
 
 export default Pokemon;
